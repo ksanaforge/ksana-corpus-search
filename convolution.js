6eb871ec9ae81ee21b3007eaf45407d0e72d426b
yapcheahshen <yapcheahshen@gmail.com>
2016-11-24 11:46:12 +0800

    boost candidate with exponentially to number of term in scope

--- convolution.js
+++ convolution.js
@@ -1,42 +1,32 @@
 const bsearch=require("ksana-corpus/bsearch");
-const MAX_CANDIDATE=50;
-const groupByLine=function(res,kposs){ 
+const MAX_CANDIDATE=50, BOOST_RATE=1.15;
 
+const groupByLine=function(res,kposs){ 
+	var byline={},out=[];
 	var matches=res.map(function(r,idx){
 			return [kposs[idx],r[1]];
 	});
-	const byline={};
 	matches.forEach(function(m){
 		const kpos=m[0];
 		if (!byline[kpos]) byline[kpos]=0;
 		if (m[1]>byline[kpos]) byline[kpos]= m[1];
 	});
-
-	var out=[];
 	for (var kpos in byline) {
 		out.push([kpos,byline[kpos]]);
 	}
-
 	out.sort(function(a,b){return b[1]-a[1];});
-
-	if (out.length>15) out.length=15;
+	if (out.length>MAX_CANDIDATE) out.length=MAX_CANDIDATE;
 	return out;
 }
 
 const mergePosting=function(postings){
-	var o=[];
-	var t=new Date();
-	for (var i=0;i<postings.length;i++) {
-		o=o.concat(postings[i]);
-	}
-	o.sort(function(a,b){return a-b});
-	return o;　
+	//slightly faster than concat one by one
+	var o=[].concat.apply([], postings); 
+	return o.sort(function(a,b){return a-b});
 }
 
 const convolutePosting=function(posting, windowsize,threshold){
-	var out=[];
-	var t=new Date();
-	
+	var out=[], t=new Date();	
 	for (var i=0;i<posting.length;i++) {
 		var j=1,sum=0;
 		while (j<windowsize&& posting[i+j]-posting[i]<windowsize ) {
@@ -49,16 +39,15 @@ const convolutePosting=function(posting, windowsize,threshold){
 	if (out.length>MAX_CANDIDATE) out.length=MAX_CANDIDATE;
 	return out;
 }
-const rankCandidate=function(candidates, windowsize,postings){
+const boostCandidate=function(candidates, windowsize,postings){
 	for (var i=0;i<candidates.length;i++) {
-		var score=0;
+		var score=1;
 		const tpos=candidates[i][0]+windowsize;
-
 		for (var j=0;j<postings.length;j++) {
 			const posting=postings[j];
 			const at=bsearch(posting,tpos,true);
-			if (tpos-posting[at-1]<=windowsize) {
-				score++;
+			if (tpos-posting[at-1]<=windowsize) { // is this term in range?
+				score*=BOOST_RATE;
 			}
 		}
 		candidates[i][1]*=score;
@@ -68,7 +57,6 @@ const rankCandidate=function(candidates, windowsize,postings){
 const preparetokens=function(res,tokens){
 		const alltokens=res[0],posting_length=res[1];
 		var tokenid_len=[],tokenexists={};;
-
 		tokens.forEach(function(tk,idx){
 			const at= bsearch(alltokens,tk);
 			if (at>-1) {
@@ -88,15 +76,13 @@ const preparetokens=function(res,tokens){
 				}
 			}
 		});
-		
 		tokenid_len=tokenid_len.filter(function(t){return t[0]!==-1});
 		return tokenid_len;
 }
 const convolutionSearch=function(cor,query,opts,cb){
 	const r=cor.tokenizer.tokenize(query);
 	const tokens=r.map(function(tk){return tk[0]});
-	var timer={},t=new Date();
-	var t1=t;
+	var timer={},t=new Date(),t1=t;
 	cor.get([["inverted","tokens"],["inverted","posting_length"]],function(res){
 		tokenid_len=preparetokens(res,tokens);
 		const totalpostinglength= tokenid_len.reduce(function(n,v){ return n+v[1] } , 0);
@@ -116,15 +102,14 @@ const convolutionSearch=function(cor,query,opts,cb){
 			timer.loadposting=new Date()-t1;t1=new Date();
 			const hits=mergePosting(postings);
 			timer.mergeposting=new Date()-t1;t1=new Date();
-			const candidates=convolutePosting(hits, windowsize, threshold );
-			const result=rankCandidate(candidates,windowsize,postings);
+			var candidates=convolutePosting(hits, windowsize, threshold );
+			candidates=boostCandidate(candidates,windowsize,postings);
 			timer.convolution=new Date()-t1;t1=new Date();			
-			const tposs=result.map(function(r){return r[0]});
+			const tposs=candidates.map(function(r){return r[0]});
 			cor.fromTPos(tposs,function(kposs){
 				timer.tokpos=new Date()-t1;t1=new Date();
-				const matches=groupByLine(result,kposs);
+				const matches=groupByLine(candidates,kposs);
 				timer.total=new Date()-t;
-
 				const terms=tokenid_len.map(function(tk){return tk[2]}).join(" ");
 				cb&&cb({matches, timer , terms, hits: hits.length});
 			})
